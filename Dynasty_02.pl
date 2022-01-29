ba6a459f97ce058ad424def7c6e063c10ae5770b
Marcin Czeszowic <marcinczeszowic@gmail.com>
2022-01-29 14:37:44 +0100

    zaliczenie zadania nr 3 Marcin Czeszowic 115382

--- Dynasty_02.pl
+++ Dynasty_02.pl
@@ -10,10 +10,11 @@ bona.
 karol_V_madry.
 joanna_de_burbon
 ludwik_I.
-maria.
+maria_corka_Karola_de_biois.
 filip_III_smialy.
 malgorzata_flandryjska.
 
+/* miot karola v madry */
 karol_VI_szalony.
 izabela_bawarska.
 karol_VII.
@@ -26,7 +27,54 @@ ludwik_XII.
 klaudia_walezjuszka.
 franciszek_I.
 
+ludwik.
+walentyna.
+karol.
+maria_z_rodu_ksiazat_kliwii.
+jan.
+zona_jana.
+karol_hr_angoleme.
+ludwika_sabudzka. 
+malgorzata_kr_nawarry.
+henryk.
+joanna_d_albert.
+antoni_de_burbon
+henryk_IV.
+malgorzata_walezjuszka.
 
+henryk_II.
+katarzyna_medycejska.
+
+franciszek_II.
+maria_stuart.
+karol_IX.
+elzbieta_hadsburg.
+henryk_III.
+ludwika_lotarynska.
+
+/* ludwika I */
+
+ludwik_II.
+jolanta.
+maria.
+ludwik_III.
+Rene_I.
+izabela_lotarynska.
+jolanta_ks_lotaryngii.
+jan_II.
+maria_corka_Karola_I_de_bourbon
+mikolaj_I.
+
+/* miot filipa II */
+
+jan_bez_trwogi.
+malgorzata_bawarska.
+filip_III_dobry.
+izabela_corka_jana_I.
+karol_zuchwaly.
+izabela_corka_karola_de_bourbon.
+maria_ks_burgundii.
+maksymilian_I_Habsburg_cesarz.
 
 /*  parent ( child, parent).  */
 parent(filip_VI,karol_de_Valois).
@@ -42,22 +90,61 @@ parent(karol_VI_szalony,joanna_de_burbon).
 parent(karol_VII,karol_VI_szalony).
 parent(karol_VII_szalony,izabela_bawarska).
 parent(ludwik_XI,karol_VII).
-parent(ludwik_XI,marta).
+parent(ludwik_XI,maria).
 parent(karol_VIII,ludwik_XI).
 parent(karol_VIII,karolina_sabudzka).
+parent(klaudia_walezjuszka,anna).
+parent(klaudia_walezjuszka,ludwik_XII).
+parent(henryk_II,klaudia_walezjuszka).
+parent(henryk_II,franciszek_I).
+parent(franciszek_II,henryk_II).
+parent(franciszek_II,katarzyna_medycejska).
+parent(karol_IX,henryk_II).
+parent(karol_IX,katarzyna_medycejska).
+parent(henryk_III,henryk_II).
+parent(henryk_III,katarzyna_medycejska).
+parent(malgorzata_walezjuszka,henryk_II).
+parent(malgorzata_walezjuszka,katarzyna_medycejska).
 
+parent(franciszek_I,karol_hr_angoleme).
+parent(franciszek_I,ludwika_sabudzka).
 
+parent(malgorzata_kr_nawarry,karol_hr_angoleme).
+parent(malgorzata_kr_nawarry,ludwika_sabudzka).
+parent(joanna_d_albert,malgorzata_kr_nawarry).
+parent(joanna_d_albert,henryk_III).
+parent(henryk_IV,joanna_d_albert).
+parent(henryk_IV,antoni_de_burbon).
 
 
 parent(ludwik_I,jan_II_dobry).
 parent(ludwik_I,bona).
+parent(ludwik_II,ludwik_I).
+parent(ludwik_II,maria).
+parent(maria,ludwik_II).
+parent(maria,jolanta).
+parent(ludwik_III,ludwik_II).
+parent(ludwik_III,jolanta).
+parent(Rene_I,ludwik_II).
+parent(Rene_I,jolanta).
+parent(jolanta_ks_lotaryngii,Rene_I).
+parent(jolanta_ks_lotaryngii,izabela_lotarynska).
+parent(jan_II,Rene_I).
+parent(jan_II,izabela_lotarynska).
+parent(mikolaj_I,jan_II).
+parent(mikolaj_I,maria_corka_Karola_I_de_bourbon)
+
 
 parent(filip_III_smialy,jan_II_dobry).
 parent(filip_III_smialy,bona).
-
-
-
-
+parent(jan_bez_trwogi,filip_III_smialy).
+parent(jan_bez_trwogi,malgorzata_flandryjska).
+parent(filip_III_dobry,jan_bez_trwogi).
+parent(filip_III_dobry,malgorzata_bawarska)
+parent(karol_zuchwaly,filip_III_dobry).
+parent(karol_zuchwaly,izabela_corka_jana_I).
+parent(maria_ks_burgundii,karol_zuchwaly).
+parent(maria_ks_burgundii,izabela_corka_karola_de_bourbon).
 
 
 /* male */
@@ -74,13 +161,27 @@ male(ludwik_XI).
 male(karol_VIII).
 male(ludwik_XII).
 male(franciszek_I).
-male().
-male().
-male().
-male().
-male().
-male().
-male().
+male(henryk_II).
+male(ludwik).
+male(karol).
+male(jan).
+male(karol_hr_angoleme).
+male(henryk).
+male(antoni_de_burbon).
+male(henryk_IV).
+male(franciszek_II).
+male(karol_IX).
+male(henryk_III).
+male(ludwik_II).
+male(ludwik_III).
+male(Rene_I).
+male(jan_II).
+male(mikolaj_I).
+male(jan_bez_trwogi).
+male(filip_III_dobry).
+male(karol_zuchwaly).
+male(maksymilian_I_Habsburg_cesarz).
+
 
 
 /* female */
@@ -89,20 +190,34 @@ female(malgorzata).
 female(joanna).
 female(bona).
 female(joanna_de_burbon).
-female(maria).
+female(maria_corka_Karola_de_biois).
 female(malgorzata_flandryjska).
 female(izabela_bawarska).
 female(marta).
 female(karolina_sabudzka).
 female(anna).
 female(klaudia_walezjuszka).
-female().
-female().
-female().
-female().
-female().
-female().
-female().
-female().
+female(walentyna).
+female(maria_z_rodu_ksiazat_kliwii).
+female(zona_jana).
+femaleludwika_sabudzka).
+female(malgorzata_kr_nawarry).
+female(joanna_d_albert).
+female(malgorzata_walezjuszka).
+female(maria_stuart).
+female(elzbieta_hadsburg).
+female(ludwika_lotarynska).
+female(jolanta).
+female(maria).
+female(izabela_lotarynska).
+female(jolanta_ks_lotaryngii).
+female(maria_corka_Karola_I_de_bourbon).
+female(malgorzata_bawarska).
+female(izabela_corka_jana_I).
+female(izabela_corka_karola_de_bourbon).
+female(maria_ks_burgundii).
+
+
+
 
 
